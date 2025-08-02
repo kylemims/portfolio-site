dd1593a6866454dacef5475c280039098d5a6be3
Kyle Mims <kylemims@gmail.com>
2025-08-02 08:46:13 -0500

    fixed link addresses

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,29 +16,6 @@ export const Hero = () => {
           </a>
         </div>
       </section>
-
-      {/* <section className="intro">
-        <div className="intro-container">
-          <div className="intro-image">
-            <img src={headshot} alt="Kyle Mims smiling" />
-          </div>
-          <div className="intro-text">
-            <h3>Let’s Make Something Cool Together</h3>
-            <p>
-              I’m a former creative entrepreneur turned software developer with a deep love for bold design,
-              clean UI, and building things that make people say “whoa.” Whether I’m building pizza apps or
-              park planners, I’m obsessed with the details and always thinking about the user experience.
-            </p>
-            <p>
-              This site is built with intention—from the projects to the pixels. I’d love your feedback,
-              ideas, or just a hello.
-            </p>
-            <a className="feedback-button" href="mailto:kylemims.dev@gmail.com">
-              Send Me Feedback
-            </a>
-          </div>
-        </div>
-      </section> */}
     </>
   );
 };
